c4af7ad6900daf17a3cdbfe84c3654752a56574e
Andeechu <andychuabsurd@gmail.com>
2021-01-08 17:32:31 -0800

    added examples to puppeteer_test.js

--- puppeteer_test.js
+++ puppeteer_test.js
@@ -1,10 +1,23 @@
 const puppeteer = require('puppeteer');
 const url = 'https://nodejs.org/en/blog/';
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://nodejs.org/en/blog/');
-  await page.screenshot({path: 'nodejs_blog_screenshot.png'});
- 
-  await browser.close();
-})();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const response = await page.goto(url)
+
+    // Example: Taking a screenshot
+    // await page.screenshot({path: 'nodejs_blog_screenshot.png'});
+
+    // Example: Printing all html text within page
+    // console.log(await response.text());
+
+    // Example: Getting element using XPath (not full Xpath)
+    const example = await page.evaluate(element => {
+        return element.textContent;
+      }, (await page.$x('//*[@id="main"]/div/ul/li[1]/a'))[0]);
+    console.log(example);
+
+
+
+    await browser.close();
+}) ();

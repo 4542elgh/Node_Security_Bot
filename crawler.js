47cc29a2164830be0ebfff43cb2cf09ca84b98c3
4542elgh <4542elgh@gmail.com>
2021-01-17 22:04:00 -0800

    Add commentary

--- crawler.js
+++ crawler.js
@@ -2,22 +2,19 @@ const request = require("request");
 const cheerio = require("cheerio");
 const db = require("./db.js");
 
+// Export as function
 const fetchBlogs = (sendToChannel) => {
   request("https://nodejs.org/en/blog", (err, res, html) => {
     if (!err && res.statusCode == 200) {
       const $ = cheerio.load(html);
-
-      // array to store announcements
       let announcements = [];
 
-      // Getting HTLM list and iterating through items
       $("ul.blog-index")
         .children()
         .each(function (i, element) {
           const announced = $($(this).children()[0]).text();
           const entry = $($(this).children()[1]);
           const title = entry.text();
-
           let description = "";
 
           $($(this).children()[2])
@@ -43,20 +40,12 @@ const fetchBlogs = (sendToChannel) => {
               }
             });
 
-          const link = `https://nodejs.org${entry.attr("href")}`;
-
-          // creating announcement
-          const announcement = {
-            // id: i,
+          announcements.push({
             announced: announced,
             title: title,
-            link: link,
+            link: `https://nodejs.org${entry.attr("href")}`,
             description: description,
-            path: `${title}.html`,
-          };
-
-          // adding announcement
-          announcements.push(announcement);
+          });
         });
 
       announcements = announcements.slice(0, 2);
@@ -65,12 +54,12 @@ const fetchBlogs = (sendToChannel) => {
         request(a.link, (err, res, page) => {
           const c = cheerio.load(page);
 
+          // This loop is ok to be synchronized, code after this block will still run as expected
           c("#main")
             .find("div > div")
             .children()
             .each(function (i, element) {
-              // This is one level deeper than last commit. Correctly display blog title and title only
-              // const blog_title = c(c(c(this).children()[0]).children()[0]).text();
+              // Display blog title and title only
               const blog_author = c(c(c(this).children()[0]).children()[1])
                 .text()
                 .substring(3) // remove the 'by ' string in the front
@@ -87,14 +76,13 @@ const fetchBlogs = (sendToChannel) => {
 
           const dbConnection = new db();
 
+          // This is a callback, you cannot do something after this function and rely on values inside async function. It will be undefined
           dbConnection.lastEntry(function (result) {
             const last_entry = result[0];
-
             if (a.title !== last_entry.title) {
               console.log("update database");
               // dbConnection.insertIntoBlog(latest_entries.slice(0, 1));
               console.log("Inserted new entries");
-              console.log(a.title);
               sendToChannel(a.title);
             } else {
               console.log("No update necessary");
--- index.js
+++ index.js
@@ -1,42 +1,28 @@
-// // Supply your own discord bot token in dotenv
+// Supply your own discord bot token in dotenv
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const CronJob = require("cron").CronJob;
 const fetchBlog = require("./crawler.js");
 
-// const db = require("./db.js");
-
-// const prefix = "!";
-
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  // console.log(client.channels)
 });
 
+// Pass function to another class
 const sendToChannel = (msg) => {
   client.channels.fetch(`${process.env.CHANNEL_ID}`).then((channel) => {
     channel.send(msg);
   });
 };
 
-// // When discord received a message
-// client.on("message", (msg) => {
-//   // Only reply when the message starts with prefix command
-//   if (!message.content.startsWith(prefix)) return;
-
-//   if (msg.content === prefix + "ping") {
-//     msg.reply("Pong!");
-//   }
-// });
-
-// // Login with your discord token
+// Login with your discord token
 client.login(`${process.env.D_TOKEN}`);
 
 // Schedule the cron job
 var job = new CronJob(
   // Every hour "0 * * * *"
-  // Below is every min
+  // Below is every 10 min
   "*/10 * * * * *",
   function () {
     fetchBlog(sendToChannel);

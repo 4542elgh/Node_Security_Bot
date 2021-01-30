5c351ad1fe665d1452936846b77ca3fcbc27f8c6
Andeechu <andychuabsurd@gmail.com>
2021-01-29 17:51:58 -0800

    created a way to extract bulleted list

--- crawler.js
+++ crawler.js
@@ -133,13 +133,19 @@ const fetchBlogs = (sendToChannel) => {
           let cve = -1; // record the current cve index, this only apply to arrays with CVE as first 3 letter
           let marker = -1; //record header plain text if CVE is present. Plain text will be from index 0 to first occurance of CVE string
           description["sublist"] = [];
-          const descriptionObject = { sublist: [], header: [] };
+          const descriptionObject = { sublist: [], header: [] , bullets: {}};
 
+          let colon = -1;
+          let start = -1;
+          const bulletTextObject = { word: '', bullets: []}
+    
           description.forEach((item, index) => {
+            // console.log(item)
             if (item.substring(0, 3) == "CVE") {
               if (cve == -1) {
                 marker = index;
-              } else {
+              } 
+              else {
                 descriptionObject["sublist"].push(
                   description.slice(cve, index).join("")
                 );
@@ -153,13 +159,45 @@ const fetchBlogs = (sendToChannel) => {
                 description.slice(cve, description.length).join("")
               );
             }
+
+            // Getting text if word ends with colon excluding "Vulnerabilities fixed"
+            if (item.substring(item.length - 1) === ':' && item !== 'Vulnerabilities fixed:') {
+              if (colon > -1) {
+                if (start == index - 1){
+                  bulletTextObject['word'] = description[colon]
+                  bulletTextObject['bullets'] = [description[start]]
+                } else {
+                  bulletTextObject['word'] = description[colon]
+                  bulletTextObject['bullets'] = description.slice(start, index)
+                }
+                console.log(bulletTextObject)
+              }
+              colon = index
+              start = index + 1
+            }
+
+            // end of string
+            if (description.length - 1 == index && colon > - 1 && item !== 'Vulnerabilities fixed:'){
+              if (start == index){
+                bulletTextObject['word'] = description[colon]
+                bulletTextObject['bullets'] = [description[start]]
+              } else {
+                bulletTextObject['word'] = description[colon]
+                bulletTextObject['bullets'] = description.slice(start, index + 1)
+              }
+              console.log(bulletTextObject)
+              colon = -1
+              start = -1
+            }
+
           });
+
 
           if (marker != -1) {
             descriptionObject["header"] = description.slice(0, marker);
           }
 
-          console.log(descriptionObject);
+          // console.log(descriptionObject);
 
           announcements.push({
             announced: announced,
